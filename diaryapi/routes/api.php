f251e2f5a2fa6830c1c84f0959ac0eb6354336cb
JohnMakgakgaDS <johnmakgakgads@gmail.com>
2025-11-02 07:25:33 +0200

    free daily  diary

--- diaryapi/routes/api.php
+++ diaryapi/routes/api.php
@@ -2,28 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\DiaryEntryController;
 
 
 
 
 
-// ✅ Add this line if missing
-Route::post('/register', [AuthController::class, 'register']);
 
-Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/diaries', [DiaryController::class, 'index']);
+    Route::post('/diaries', [DiaryController::class, 'store']);
+    Route::get('/diaries/{id}', [DiaryController::class, 'show']);
+    Route::put('/diaries/{id}', [DiaryController::class, 'update']);
+    Route::delete('/diaries/{id}', [DiaryController::class, 'destroy']);
 });
-
-
-
-
-Route::get('entries', [DiaryEntryController::class, 'index']);
-Route::post('entries', [DiaryEntryController::class, 'store']);
-Route::delete('entries/{id}', [DiaryEntryController::class, 'destroy']);
 
 
 
--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'login_screen.dart';
 import 'diary_home_page.dart';
 
 void main() {
@@ -16,37 +15,14 @@ class DiaryApp extends StatelessWidget {
       title: 'Diary App',
       theme: ThemeData(primarySwatch: Colors.indigo),
       debugShowCheckedModeBanner: false,
-      home: const AuthCheck(),
+      home: const DiaryHomePage(),  // ← added the home widget here
     );
   }
 }
 
-class AuthCheck extends StatefulWidget {
-  const AuthCheck({super.key});
 
-  @override
-  State<AuthCheck> createState() => _AuthCheckState();
-}
 
-class _AuthCheckState extends State<AuthCheck> {
-  bool isLoggedIn = false;
 
-  @override
-  void initState() {
-    super.initState();
-    checkLoginStatus();
-  }
 
-  Future<void> checkLoginStatus() async {
-    final prefs = await SharedPreferences.getInstance();
-    final token = prefs.getString('token');
-    setState(() {
-      isLoggedIn = token != null;
-    });
-  }
+ 
 
-  @override
-  Widget build(BuildContext context) {
-    return isLoggedIn ? const DiaryHomePage() : const LoginScreen();
-  }
-}
